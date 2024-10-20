a704963dcbe05c1edb3e81166e93355689432bb3
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-10-20 01:31:28 -0300

    agregando mas tokens

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style/variables.css";
+import "./style.css";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import './home-style.css'
+
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">contendor</div>
+    </>
+  );
+};
+
+export default Home;
